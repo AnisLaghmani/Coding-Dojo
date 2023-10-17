57782abe9149fab67f6eefc9c07dddf9c6f2fb26
AnisLaghmani <anislaghmani@gmail.com>
2023-10-17 10:31:07 +0100

    rearrange

---
+++ Programming Basics/Week 1/Day 1/Optional/code.js
@@ -0,0 +1,7 @@
+// what would this variable do? a variable where we will store total number of likes
+var likeCount = 0;
+//  what would be the function of this code block? its the function that gonna increase the number of likes +1 when we call it
+function increaseLikes() {
+    //what is this sort operation for? this operation take the old value of the variable and increase it +1
+    likeCount = likeCount + 1;
+}
