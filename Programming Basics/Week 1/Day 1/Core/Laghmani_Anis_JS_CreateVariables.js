089e3d4d196ccb4f66eb656bb47270ac532d2327
AnisLaghmani <93858626+AnisLaghmani@users.noreply.github.com>
2023-10-17 10:05:46 +0100

    programming basics commit

---
+++ Programming Basics/Week 1/Day 1/Core/Laghmani_Anis_JS_CreateVariables.js
@@ -0,0 +1,6 @@
+var tallMin=42 ; // the tall variable that we will compare with
+var ageMin=10;//the age variable that we will compare with
+var ageOfCustomer=0;// the age variable that we will enter
+var tallOfCustomer=0;// the tall variable that we will enter
+var resultOK="You can ride!";//message that we will display when  the rider is tall and old enough to ride the rollercoaster.
+var resultNOK="You can't ride!Come back when you're older "//message that we will display when  the rider is not tall and/or old enough to ride
---
+++ Programming Basics/Week 1/Day 1/Core/Laghmani_anis_AlgoApp1.js
@@ -0,0 +1,22 @@
+//---------challenge1---------
+var x=5;
+console.log(x);//display 5
+//---------challenge2---------
+var x=3;
+x=x+1;
+console.log(x);//display 4
+//---------challenge3---------
+var x=3;
+x=x+2;
+console.log(x+2);//display 7
+console.log(x);// display 5
+//---------challenge4---------
+var x=3;
+x=x*x;
+console.log(x);//display 9
+console.log(x*2);//display 18
+//---------challenge5---------
+var x=2;
+var y=5;
+var z=x+y;
+console.log(z);// display 7
