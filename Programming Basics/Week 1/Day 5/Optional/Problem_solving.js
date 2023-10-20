c015c15361b4073ee9471dfd3a2c759946b2b5ec
AnisLaghmani <anislaghmani@gmail.com>
2023-10-20 09:33:36 +0100

    probSolving

---
+++ Programming Basics/Week 1/Day 5/Optional/Problem_solving.js
@@ -0,0 +1,17 @@
+//****A computer application needs to be able to identify if a written paragraph is written in First, Second, or Third Person 
+
+
+//****Using comments, write out 1 strategy that could be used to solve the problem.
+
+
+//****Using comments, create pseudocode (an informal but readable description of code that uses regular language to outline a solution) to apply one of the strategies to solve the problem.
+//i think first we need an array that takes the paragraph and put every word in elements of array 
+//second we need a for loop which crosses element (words of paragraph )of the array
+//third conditions that count up every time that the word is equal i/we or you or he/she. more details:we need three counters 
+//one counting i/we ,second one   counting (he/she) and the third counting (you).
+//and finalyy the highest counter value can   identify in which person the paragraph is written
+
+
+
+//Using comments, write a short paragraph that answers: What is the hardest part of problem-solving?
+//the hardest part is the extraction of word from paragraph to array 
