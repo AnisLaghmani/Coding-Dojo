6516923687a49dcdbd8069037b9dff586e2e5563
AnisLaghmani <anislaghmani@gmail.com>
2023-11-17 13:46:28 +0100

    playground

---
+++ Python/Week 1/Day 5/Core/playground/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template  # Import Flask to allow us to create our app
+app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+# --------------------------------------------------------------------
+
+@app.route('/play/<int:number>/<color>')          # The "@" decorator associates this route with the function immediately following
+def circles(number,color):
+    return render_template('index.html', color=color,number=number)
+
+
+
+# -------------------------------------------------------------------------
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True,port=5001)    # Run the app in debug mode.
