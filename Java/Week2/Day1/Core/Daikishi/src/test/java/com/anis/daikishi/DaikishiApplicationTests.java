17b8c7afbb7211c935fc734209095de9511e6818
AnisLaghmani <anislaghmani@gmail.com>
2024-01-29 18:31:29 +0100

    daikishiCore

---
+++ Java/Week2/Day1/Core/Daikishi/src/test/java/com/anis/daikishi/DaikishiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anis.daikishi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikishiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
